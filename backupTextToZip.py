4b440adc362189c2149102bc7996de399e2e9679
Vuyisile Ndlovu <terrameijar@gmail.com>
2017-01-26 09:21:25 +0200

    Removed TODOs, unnecessary code

--- backupTextToZip.py
+++ backupTextToZip.py
@@ -5,39 +5,35 @@
 
 import zipfile
 import os
-import re
 
 
 def backup_text_files(folder):
-    # TODO: Write a regex to match *.py and *.txt files
-    # file_extension = re.compile(r'\w+\.(txt|py)$')
-    # figure out the filename to be produced based on existing files
 
     folder = os.path.abspath(folder)
     number = 1
+
+    # Create a new zip file each time this script is run
     while True:
         zip_filename = os.path.basename(folder) + '_' + str(number) + '.zip'
-        print zip_filename
         if not os.path.exists(zip_filename):
             break
         number += 1
 
-    # TODO: Get the folder name
+    # Get the folder name
     folder = os.path.abspath(folder)  # Make sure the folder name is absolute
 
-    # TODO: Create a zip file
+    # Create a zip file
     backup_zip = zipfile.ZipFile(zip_filename,'w')
 
-    # TODO: Walk the entire folder and compress all *.py files in the directory
+    # Walk the entire folder and compress all .py / .txt files in the directory
     for foldername, subfolders, filenames in os.walk(folder):
         backup_zip.write(foldername)
 
         for filename in filenames:
-            # new_base = os.path.basename(folder) +'_'
             if filename.endswith('.py') or filename.endswith('.txt'):
                 backup_zip.write(os.path.join(foldername, filename))
 
     backup_zip.close()
     print "Done"
 
-backup_text_files("/home/terra/test/dir1")
+backup_text_files(".")
