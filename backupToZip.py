2f529aeb4f78bbf8b793f9e4738e8d6b5bea4611
Vuyisile Ndlovu <terrameijar@gmail.com>
2017-01-26 09:24:57 +0200

    Archiver, zips everything in it's path

--- backupToZip.py
+++ backupToZip.py
@@ -1,4 +1,5 @@
-#! /usr/bin/python
+#!/usr/bin/env python
+
 # backupToZip.py -- Copies and entire directory and it's contents into
 # a zip file whose filename increments
 # The default folder to be backed up is the current working directory
@@ -10,7 +11,7 @@ import os
 def backup_to_zip(folder):
     # Backup the entire contents of "folder" into a zip file.
 
-    folder = os.path.abspath(folder) # make sure folder is absolute
+    folder = os.path.abspath(folder)  # make sure folder is absolute
     # Figure out the filename this code should use based on what
     # files already exist.
     number = 1
@@ -19,6 +20,7 @@ def backup_to_zip(folder):
         if not os.path.exists(zip_filename):
             break
         number += 1
+
     # Create the zip file
     print "Creating %s ..." % (zip_filename)
     backup_zip = zipfile.ZipFile(zip_filename, 'w')
@@ -27,28 +29,18 @@ def backup_to_zip(folder):
     for foldername, subfolders, filenames in os.walk(folder):
         print "Adding files in %s..." % foldername
         # Add the current directory to the ZIP file.
+
         backup_zip.write(foldername)
         # Add all the files in this folder to the ZIP file.
         for filename in filenames:
             new_base = os.path.basename(folder) + '_'
             if filename.startswith(new_base) and filename.endswith('.zip'):
-                continue # don't backup the backup ZIP files
+                continue #  don't backup the backup ZIP files
+
             backup_zip.write(os.path.join(foldername, filename))
+
     backup_zip.close()
     print "Done"
 
 
 backup_to_zip(".")
-
-
-
-
-
-
-
-
-
-
-
-
-
