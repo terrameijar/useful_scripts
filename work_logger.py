b290d1d841e3e46e15b0b030f19761802c78de20
Vuyisile Ndlovu <terrameijar@gmail.com>
2017-03-16 18:35:45 +0200

    Added timer to make the code run once every thirty seconds

--- work_logger.py
+++ work_logger.py
@@ -2,26 +2,44 @@
 
 import os
 import datetime
+import time
 import pyperclip
 import re
+import pdb
 
+def write_to_file():
+    current_month = datetime.datetime.today().strftime("%B_%Y")
+    today_file = datetime.datetime.today().strftime("%B%d.txt")
 
-current_month = datetime.datetime.today().strftime("%B_%Y")
-today_file = datetime.datetime.today().strftime("%B%d.txt")
+    work_logs_path = '/home/terra/Documents/foxy/docs'
+    current_month_logs_dir = os.path.join(work_logs_path, current_month)
 
-work_logs_path = '/home/terra/Documents/foxy/docs'
-current_month_logs_dir = os.path.join(work_logs_path, current_month)
+    osticket_match_object = \
+        re.compile(r"""https://support.(leahscape|getfoxyproxy).(org|com)/
+                       (\w+)/(\w+).+""", re.VERBOSE)
+    clipboard_contents = pyperclip.paste()
+    try:
+        if osticket_match_object.search(clipboard_contents).group():
+            osticket_url = clipboard_contents
 
-osticket_match_object = \
-    re.compile(r"""https://support.(leahscape|getfoxyproxy).(org|com)/
-                   (\w+)/(\w+).+""", re.VERBOSE)
-clipboard_contents = pyperclip.paste()
-if osticket_match_object.search(clipboard_contents).group():
-    osticket_url = clipboard_contents
+            if not os.path.exists(current_month_logs_dir):
+                os.makedirs(current_month_logs_dir)
 
-    if not os.path.exists(current_month_logs_dir):
-        os.makedirs(current_month_logs_dir)
+            with open(os.path.join(current_month_logs_dir, today_file), 'a+') \
+                    as log_file:
+                        # Don't save duplicate data to logfile
+                        tickets = log_file.readlines()
+                        log_file.write(osticket_url + '\n')
+            print "File created"   # Logging message, remove when done
+    except AttributeError:
+        pass
+def main():
+    pdb.set_trace()
+    active = True
+    while active:
+        print "Program starting"   # Logging message, remove when done
+        write_to_file()
+        time.sleep(30)
 
-    with open(os.path.join(current_month_logs_dir, today_file), 'a') \
-            as log_file:
-                log_file.write(clipboard_contents + '\n')
+main()
+# TODO: Current code will write duplicate osticket entries to the log file. Fix
