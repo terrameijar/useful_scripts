2d80af6894979cc694959f6775d2f7d294e5e537
Vuyisile Ndlovu <terrameijar@gmail.com>
2017-03-16 19:09:25 +0200

    Added code to prevent duplicate entries and added logging support

--- work_logger.py
+++ work_logger.py
@@ -5,15 +5,19 @@ import datetime
 import time
 import pyperclip
 import re
-import pdb
+import logging
+
 
 def write_to_file():
+    # Create a file in this example format: <Month><Date>.txt
     current_month = datetime.datetime.today().strftime("%B_%Y")
     today_file = datetime.datetime.today().strftime("%B%d.txt")
 
+    # Save the log file in the work folder
     work_logs_path = '/home/terra/Documents/foxy/docs'
     current_month_logs_dir = os.path.join(work_logs_path, current_month)
 
+    # Search only for osticket URLs in the clipboard
     osticket_match_object = \
         re.compile(r"""https://support.(leahscape|getfoxyproxy).(org|com)/
                        (\w+)/(\w+).+""", re.VERBOSE)
@@ -29,17 +33,30 @@ def write_to_file():
                     as log_file:
                         # Don't save duplicate data to logfile
                         tickets = log_file.readlines()
-                        log_file.write(osticket_url + '\n')
-            print "File created"   # Logging message, remove when done
+                        found = False
+                        for ticket in tickets:
+                            if osticket_url in ticket:
+                                found = True
+                                logging.info('Duplicate URL found, skipping')
+                        if not found:
+                                log_file.write(osticket_url + '\n')
+                                logging.info('%s written to logfile'
+                                             % osticket_url)
     except AttributeError:
+        logging.warning("Clipboard does not contain osticket url, skipping")
         pass
+
+
 def main():
-    pdb.set_trace()
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
     active = True
     while active:
-        print "Program starting"   # Logging message, remove when done
+        logging.info("Loop starting")
         write_to_file()
+        logging.info("Loop complete. Restarting after 30 secs")
         time.sleep(30)
 
 main()
-# TODO: Current code will write duplicate osticket entries to the log file. Fix
